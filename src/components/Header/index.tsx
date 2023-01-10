8974cb7692180bbbc351732fd58092c564c7bc05
Bruno Zolini <bruno.zolini@hotmail.com>
2023-01-10 15:01:15 -0300

    Initial commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Início</li>
+          <li>Sobre mim</li>
+          <li>Projetos</li>
+          <li>Habilidades</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
